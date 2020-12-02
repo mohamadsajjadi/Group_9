73569bce79b2a2df89c7e4e3e2079114b0f5c5e6
meshkat <meshkat.hashemian@yahoo.com>
2020-12-02 13:19:20 +0400

    box

--- src/Model/Box.java
+++ src/Model/Box.java
@@ -1,5 +1,22 @@
 package Model;
 import java.util.*;
 public class Box {
-    private Dot
+    private Dot dot1 ;
+    private Dot dot2 ;
+    private Dot dot3 ;
+    private Dot dot4 ;
+    private static ArrayList<Box> boxes = new ArrayList<>();
+
+    public Box(Dot dot1, Dot dot2, Dot dot3, Dot dot4) {
+        this.dot1 = dot1;
+        this.dot2 = dot2;
+        this.dot3 = dot3;
+        this.dot4 = dot4;
+    }
+    public static void addBox(Box box){
+
+    }
+    public static boolean isBoxMade(Line line){
+
+    }
 }
