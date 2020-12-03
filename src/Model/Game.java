8cc790d38fa6c92fd826533dfab2aef22046696f
mohamadsajjadi <alisajadian49@yahoo.com>
2020-12-03 19:42:01 +0330

    Game assumptions 2

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -1,8 +1,11 @@
 package Model;
 
+import java.util.*;
 public class Game {
     private Player p1;
     private Player p2;
+    private int turn;
+
 
 
     public void setP1(Player p1) {
@@ -17,13 +20,22 @@ public class Game {
 
     }
 
-    public int whoIsTurn() {
+    public Player whoIsTurn() {
 
-        
+
     }
 
     public void checkTable(Line line) {
+        int count = Box.howManyBoxesMade(line);
+        if (count == 0) {
+            changeTurn();
+        } else if (count == 1) {
+            whoIsTurn();
 
+        } else if(count == 2){
+            whoIsTurn();
+
+        }
 
     }
 }
